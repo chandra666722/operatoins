ec2513897533ac607c06ec89de51fcacf090cde6
very3 <very3@DESKTOP-TQ3O66M>
2026-08-20 20:49:46 +0530

    tl addtion form

---
+++ AllOperations/src/com/nt/service/Addition.java
@@ -0,0 +1,14 @@
+package com.nt.service;
+
+public class Addition {
+	void addtion(int a,int b,int c) {
+		System.out.println(a+b+c);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Addition().addtion(1,2,3);
+
+	}
+
+}
