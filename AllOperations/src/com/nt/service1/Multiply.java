e93f7750dc0988ae5b0cdbb426869157cc9ade86
very3 <very3@DESKTOP-TQ3O66M>
2026-08-20 21:07:38 +0530

    dev1 add class multply

---
+++ AllOperations/src/com/nt/service1/Multiply.java
@@ -0,0 +1,14 @@
+package com.nt.service1;
+
+public class Multiply {
+	void multiply(int a,int b) {
+		System.out.println(a*b);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Multiply().multiply(10, 10);
+
+	}
+
+}
